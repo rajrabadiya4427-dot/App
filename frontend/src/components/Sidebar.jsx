55037958f4c01c5a948f5ed794ebee8d30e7abdc
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-04-17 14:46:14 +0530

    Initial commit

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -29,7 +29,7 @@ const filteredUsers = Array.isArray(users)
   if (isUsersLoading) return <SidebarSkeleton />;
 
   return (
-    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+    <aside className="h-full w-full border-base-300 flex flex-col transition-all duration-200">
       <div className="border-b border-base-300 w-full p-5">
         <div className="flex items-center gap-2">
           <User2Icon className="size-6" />
@@ -84,7 +84,7 @@ const filteredUsers = Array.isArray(users)
           </button>
         </div>
       )}
-      
+
 
       <div className="overflow-y-auto w-full py-3">
         {filteredUsers.map((user) => (
