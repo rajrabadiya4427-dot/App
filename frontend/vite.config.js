1ab45ad3a06b82fdcbd04531c4213dbab73e357d
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-04-17 14:13:14 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+     build: {
+    // Set the limit to 1000 KiB
+    chunkSizeWarningLimit: 1000,
+  },
 })
