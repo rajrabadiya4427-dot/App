9a74e80c88ebd344d43f0c81c17f053e3a9a8544
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-04-18 13:15:16 +0530

    Initial commit

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import { useAuthStore } from "./useAuthStore";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "dark",
+  theme: localStorage.getItem("chat-theme") || "light",
   setTheme: (theme, receiverId) => {
     localStorage.setItem("chat-theme", theme);
       document.documentElement.setAttribute("data-theme", theme);
