d5c1c5fdd6420c4db07824bdf08afb6f482638ef
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-04-18 13:07:50 +0530

    Initial commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 ">
-        <div className="bg-base-100 rounded-lg w-full shadow-cl  h-[calc(100vh-8rem)] overflow-hidden">
+        <div className="bg-base-100 rounded-lg w-full shadow-cl  min-h-[calc(100vh-8rem)] overflow-hidden">
           {!selectedUser ? <Sidebar /> : <ChatContainer />}
         </div>
       </div>
