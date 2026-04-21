4acd9fb62a9f5491dbf8ec77fdb6d677efd553ef
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-04-21 12:50:23 +0530

    Initial commit

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -56,7 +56,7 @@ const ChatContainer = () => {
       <ChatHeader />
 
       <div
-        className="flex-1 overflow-y-auto p-4 space-y-4 chatwall hide-scrollbar"
+        className="flex-1 overflow-y-auto p-4 space-y-4 chatwall"
         style={{
           backgroundImage: wallpaper ? `url(${wallpaper})` : "none",
           backgroundSize: "cover",
