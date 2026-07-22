58c4d5466ef94dbd64f94efd7c5115beb184bf88
Raj Rabadiya <YOUR_NOREPLY_EMAIL@users.noreply.github.com>
2026-07-22 14:49:30 +0530

    Updated project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-     build: {
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "Chat App",
+        short_name: "App",
+        description: "My MERN Application",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+
+        icons: [
+          {
+            src: "/chaticon.jpg",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/chaticon.jpg",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+    build: {
     // Set the limit to 1000 KiB
     chunkSizeWarningLimit: 1000,
   },
-})
+});
