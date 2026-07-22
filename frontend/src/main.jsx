a7ffcc45e10147527c4a0e89b79d7e53f17b5772
Raj Rabadiya <YOUR_NOREPLY_EMAIL@users.noreply.github.com>
2026-07-22 14:51:33 +0530

    add pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { registerSW } from "virtual:pwa-register";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -12,3 +13,6 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </StrictMode>
 );
+registerSW({
+  immediate: true,
+});
