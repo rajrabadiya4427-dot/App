34aa83213fdb8402953b3a49f4d461507bf021cc
Raj Rabadiya <YOUR_NOREPLY_EMAIL@users.noreply.github.com>
2026-07-22 15:08:41 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
 
         icons: [
           {
-            src: "/chaticon.jpg",
+            src: "/icon-192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/chaticon.jpg",
+            src: "/icon-512.png",
             sizes: "512x512",
             type: "image/png",
           },
